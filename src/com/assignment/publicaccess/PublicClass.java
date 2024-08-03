f6b98b28702f7cb7ee9976774d636ecac99e1f62
Hirak7076 <hirakgoswami7076@gmail.com>
2024-08-03 16:38:51 +0530

    first commit

---
+++ src/com/assignment/publicaccess/PublicClass.java
@@ -0,0 +1,9 @@
+package com.assignment.publicaccess;
+
+public class PublicClass {
+
+	public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+	
+}
---
+++ src/com/assignment/publicaccess/TestPublic.java
@@ -0,0 +1,9 @@
+package com.assignment.publicaccess;
+
+public class TestPublic {
+
+	public static void main(String[] args) {
+        PublicClass publicClassInstance = new PublicClass();
+        publicClassInstance.publicMethod();
+    }
+}
